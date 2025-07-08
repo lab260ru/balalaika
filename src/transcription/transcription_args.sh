ec2c5a48ded79af9cdea8e3b21b4cadc9a0ad469
Nikita <nicitacom2017@gmail.com>
2025-07-08 12:39:40 +0000

    feat: add timestamps and CTC+LM (SOTA) transcription

--- src/transcription/transcription_args.sh
+++ src/transcription/transcription_args.sh
@@ -14,11 +14,15 @@ activate_venv ".dev_venv"
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-PODCASTS_PATH="../../../balalaika"
-NUM_WORKERS=4
-MODEL_NAME="rnnt"
+PODCASTS_PATH="/home/nikita/Balalaika100H"
+NUM_WORKERS=1
+MODEL_NAME="ctc"
+LM_PATH="/home/nikita/yapoddataset/ru.lm.bin"
+WITH_TIMESTAMPS=True
 
 python -m src.transcription.transcription \
     --podcasts_path "$PODCASTS_PATH" \
     --num_workers "$NUM_WORKERS" \
-    --model_name "$MODEL_NAME"
+    --model_name "$MODEL_NAME" \
+    --lm_path "$LM_PATH" \
+    --with_timestamps "$WITH_TIMESTAMPS" 
